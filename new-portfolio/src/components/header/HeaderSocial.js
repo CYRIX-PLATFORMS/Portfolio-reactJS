7068575720f5ff00622ffbde0269aa68e40de4d2
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-15 07:24:56 +0100

    .

--- new-portfolio/src/components/header/HeaderSocial.js
+++ new-portfolio/src/components/header/HeaderSocial.js
@@ -1,19 +1,15 @@
 import React from 'react'
-// import {BsLinkedin} from 'react-icons/bs'
-// import {FaGithub} from 'react-icons/fa'
-// import {FaFacebookF} from 'react-icons/fa'
-// import {CgTwitter} from 'react-icons/cg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const HeaderSocial = () => {
-    // return (
-    //     // <div className='header__social'>
-    //     // <a href='https://linkedin.com' target='blank'><BsLinkedin/> </a>
-    //     // <a href='https://github.com' target='blank'><FaGithub/> </a>
-    //     // <a href='https://facebook.com' target='blank'><FaFacebookF/> </a>
-    //     // <a href='https://twitter.com' target='blank'><CgTwitter/> </a>
-    //     // </div>
-    // )
-
-
+    return (
+        <div className='header__social'>
+        <a href='https://linkedin.com' target='blank'> <FontAwesomeIcon icon="fa-linkedin"></FontAwesomeIcon></a>
+        <a href='https://github.com' target='blank'><FontAwesomeIcon icon="fa-linkedin"></FontAwesomeIcon> </a>
+        <a href='https://facebook.com' target='blank'><FontAwesomeIcon icon="fa-linkedin"></FontAwesomeIcon> </a>
+        <a href='https://twitter.com' target='blank'><FontAwesomeIcon icon="fa-linkedin"></FontAwesomeIcon> </a>
+        </div>
+    )
 }
 
 export default HeaderSocial
