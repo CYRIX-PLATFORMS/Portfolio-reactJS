ce8d15580f67e919542b781dfcc89ddd0324f766
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-15 20:38:51 +0100

    /\

--- new-portfolio/src/App.js
+++ new-portfolio/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import './index.css'
+import './App.css'
 import Header from './components/header/Header'
 import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Testimonial from './components/testimonial/Testimonial'
-import Contact from "./components/contact/Contact";
+import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
-
-
 
 
 export const App = () => {
@@ -17,16 +16,18 @@ export const App = () => {
     <>
       <Header/>
       <Navbar/>
+      <About/>
       <Experience/>
       <Services/>
       <Portfolio/>
       <Testimonial/>
       <Contact/>
-      <Footer/>
+      <Footer/>`
+
     </>
   );
 }
 
 
-export default App
+export default App;
 
--- new-portfolio/src/index.js
+++ new-portfolio/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
+import './components/header/Header'
 
-ReactDOM.render(<App/> , document.getElementById('root'));
+ReactDOM.render(
+    <App /> , 
+    document.querySelector('#root')
+);
 
