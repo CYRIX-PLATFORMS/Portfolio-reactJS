378488267bc739a0c56047761635c9a9790555d9
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-16 12:21:05 +0100

    Complete Client Portfolio

--- new-portfolio/src/App.jsx
+++ new-portfolio/src/App.jsx
@@ -1,33 +1,29 @@
 import React from "react";
-import './index.css'
-import Header from './components/header/Header'
-import Navbar from './components/navbar/Navbar'
-import About from './components/about/About' 
-import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
-import Portfolio from './components/portfolio/Portfolio'
-import Testimonial from './components/testimonial/Testimonial'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
-
-
+import "./index.css";
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonial from "./components/testimonial/Testimonial";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-
-      <Header/>
-      <Navbar/>
-      <About/>
-      <Experience/>
-      <Services/>
-      <Portfolio/>
-      <Testimonial/>
-      <Contact/>
-      <Footer/>
-
+      <Header />
+      <Navbar />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </>
   );
 }
 
-export default App; 
+export default App;
