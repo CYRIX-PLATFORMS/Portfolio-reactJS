3b30d58407599e5e073809979b32319324fe2b02
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-16 08:21:00 +0100

    files renamed

--- new-portfolio/src/components/header/Header.jsx
+++ new-portfolio/src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './header.css'
 import './CTA'
-import CTA from './CTA' //here we import the CTA component inside our header
+import CTA from './CTA' 
 import me from '../../assets/me.jpg'
-import HeaderSocial from './HeaderSocial' //import of headerSocial component
+import HeaderSocial from './HeaderSocial' 
 
 
 export const Header = () => {
