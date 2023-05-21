908f9e44b979943226ff030cf2d05ad7b76576b7
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-21 21:49:11 +0100

    About me section

--- new-portfolio/src/components/about/About.jsx
+++ new-portfolio/src/components/about/About.jsx
@@ -1,60 +1,55 @@
-import React from 'react'
-import './about.css'
-import moi from '../../assets/me.jpg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import React from "react";
+import "./about.css";
+import moi from "../../assets/me.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-    return (
-      <section id="about">
-        <h5>Get to know</h5>
-        <h2>About me</h2>
-        <div className="container about__container">
-          <div className="about__me">
-            <div className="about__me__image">
-              <img src={moi} alt="Brown " />
-            </div>
-          </div>
-          <div className="about__me__content">
-            <div className="about__cards">
-              <article className="about__card">
-                <FaAward className="about__icon" />
-                <h5>Experience</h5>
-                <small>1+ Years</small>
-              </article>
-              <article className="about__card">
-                <FiUsers className="about__icon" />
-                <h5>Clients</h5>
-                <small>3 world widely</small>
-              </article>
-              <article className="about__card">
-                <VscFolderLibrary className="about__icon" />
-                <h5>Projects</h5>
-                <small>2 Completed</small>
-              </article>
-            </div>
-            <p>
-              I am a young software engineer trained at the Faculty of
-              Engineering and Technology(FET) of the University of Buea . I get
-              my B.eng in 2021. Being on demand for experience, I apply to
-              Camsol.io which accepts me and gives me the chance to work on real
-              business projects in order to gain experience. From July 2021 to
-              today, I learned technologies such as react, react native, in
-              short the MERN stack, I am still learning and counting on the
-              support of Camsol to increase my knowledge at end of being an
-              engineer not only graduate, but also qualified. I hold a
-              French-speaking baccalaureate D sexion, my university studies at
-              FET I did in English. I can therefore express myself in French and
-              in English (average).
-            </p>
-            <a href="#contact" className="btn btn-primary">
-              Let's talk
-            </a>
+  return (
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me__image">
+            <img src={moi} alt="Brown " />
           </div>
         </div>
-      </section>
-    );
-}
+        <div className="about__me__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>2 years</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>1 in Cameroon</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+          <p>
+            I am currently a student in the University of Buea studying Computer Engineering in the Faculty
+            of Engineering and Technology and an aspiring Full Stack developer. I have expertise in Web Programming, debugging and software testing,
+            project reporting and presentation. I am really passionate about grooming and developing new skills 
+            as time goes by in my career. 
+            I have dealt with programming languages such as C, Cpp and Java. And equally, I can confidently say
+            I have expertise in HTML, CSS and Javascript and this portfolio was created using the React library 
+            which utilises the JSX syntax. 
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
