9141c8383d85f69b504873c49b7bf9f92b855965
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-25 23:45:18 +0100

    .navigation class added to nav element for modification in navbar.css

--- new-portfolio/src/components/navbar/Navbar.jsx
+++ new-portfolio/src/components/navbar/Navbar.jsx
@@ -10,41 +10,46 @@ import { useState } from "react";
 const Navbar = () => {
   const [activeNav, setActiveNav] = useState("#");
   return (
-    <nav>
+    <nav className="navigation">
       <a
         href="#Top"
         onClick={() => setActiveNav("#")}
         className={activeNav === "#" ? "active" : ""}
       >
-        <AiOutlineHome />
+        <h3>Home</h3>
+        {/* <AiOutlineHome /> */}
       </a>
       <a
         href="#about"
         onClick={() => setActiveNav("#about")}
         className={activeNav === "#about" ? "active" : ""}
       >
-        <AiOutlineUser />
+        <h3>About</h3>
+        {/* <AiOutlineUser /> */}
       </a>
       <a
         href="#experience"
         onClick={() => setActiveNav("#experience")}
         className={activeNav === "#experience" ? "active" : ""}
       >
-        <BiBook />
+        <h3>Experience</h3>
+        {/* <BiBook /> */}
       </a>
       <a
-        href="#service"
+        href="#services"
         onClick={() => setActiveNav("#service")}
         className={activeNav === "#service" ? "active" : ""}
       >
-        <RiServiceLine />
+        <h3>Services</h3>
+        {/* <RiServiceLine /> */}
       </a>
       <a
         href="#contact"
         onClick={() => setActiveNav("#contact")}
         className={activeNav === "#contact" ? "active" : ""}
       >
-        <BiMessageSquareDetail />
+        <h3>Contact</h3>
+        {/* <BiMessageSquareDetail /> */}
       </a>
     </nav>
   );
