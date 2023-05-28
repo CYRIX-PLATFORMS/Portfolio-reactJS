04e28ad38cf9c3231af6a3c2b7d7da9b049aa70a
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-28 22:28:28 +0100

    More modifications on experience section

--- new-portfolio/src/components/experience/Experience.jsx
+++ new-portfolio/src/components/experience/Experience.jsx
@@ -10,14 +10,78 @@ export const Experience = () => {
       <h2>My Experience</h2>
       <div className="container experience__container">
         <div className="experience__frontend">
-          <h3>Frontend Development</h3> 
+          <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Advanced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
+              <div>
+                <h4>CSS & SCSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
+              <div>
+                <h4>Vanilla JavaScript</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
           </div>
         </div>
-        <div className="experience__backend"></div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
+              <div>
+                <h4>Node.js</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
+              <div>
+                <h4>PHP</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"></BsPatchCheckFill>
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
         <ListExperiences />
       </div>
     </section>
