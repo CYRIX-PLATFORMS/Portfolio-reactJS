3e08f860fca2668769a2142b3543149d20ce11df
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-29 00:18:24 +0100

    Services section complete

--- new-portfolio/src/components/services/Services.jsx
+++ new-portfolio/src/components/services/Services.jsx
@@ -24,24 +24,6 @@ export const Services = () => {
                 atque, dicta quas ipsam distinctio laudantium optio?
               </p>
             </li>
-            <li>
-              <FaCheckCircle className="service__list-icon"></FaCheckCircle>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-                est, corrupti inventore error veritatis exercitationem eaque
-                doloremque? Quos nobis dolorem facilis! Consequuntur corrupti
-                atque, dicta quas ipsam distinctio laudantium optio?
-              </p>
-            </li>{" "}
-            <li>
-              <FaCheckCircle className="service__list-icon"></FaCheckCircle>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-                est, corrupti inventore error veritatis exercitationem eaque
-                doloremque? Quos nobis dolorem facilis! Consequuntur corrupti
-                atque, dicta quas ipsam distinctio laudantium optio?
-              </p>
-            </li>
           </ul>
         </article>
         <article className="service">
@@ -66,7 +48,14 @@ export const Services = () => {
                 doloremque? Quos nobis dolorem facilis! Consequuntur corrupti
                 atque, dicta quas ipsam distinctio laudantium optio?
               </p>
-            </li>{" "}
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Social Media Management</h3>
+          </div>
+          <ul className="service__list">
             <li>
               <FaCheckCircle className="service__list-icon"></FaCheckCircle>
               <p>
