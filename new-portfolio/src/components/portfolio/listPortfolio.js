3f9b6800fa11a433cd23266cb39893654a3a248c
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-05-30 11:26:51 +0100

    working on backend for list portfolio

--- new-portfolio/src/components/portfolio/listPortfolio.js
+++ new-portfolio/src/components/portfolio/listPortfolio.js
@@ -1,21 +1,17 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const ListPortfolio = () => {
-    const [data, setData] = useState([])
-    useEffect(()=>{
-        getPortfolio()
-    }, []);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getPortfolio();
+  }, []);
 
-    const getPortfolio = async() =>{
-        const response = await axios.get("/api/project/projects");
-        setData(response.data.project)
-    }
-    console.log(data)
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const getPortfolio = async () => {
+    const response = await axios.get("/api/project/projects");
+    setData(response.data.project);
+  };
+  console.log(data);
+  return <div></div>;
+};
 
 export default ListPortfolio;
