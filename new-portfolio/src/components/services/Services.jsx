2cec947c8e159d0063795761ccd33a0a65aed127
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-06-02 12:40:34 +0100

    scripting of backend files and modules

--- new-portfolio/src/components/services/Services.jsx
+++ new-portfolio/src/components/services/Services.jsx
@@ -9,8 +9,8 @@ export const Services = () => {
       <h5>Here are a list of services I offer</h5>
       <h2>Services</h2>
       <div className="container services__container">
-        {/* <ListServices /> */}
-        <article className="service">
+        <ListServices />
+        {/* <article className="service">
           <div className="service__head">
             <h3>UI/UX Design</h3>
           </div>
@@ -66,7 +66,7 @@ export const Services = () => {
               </p>
             </li>
           </ul>
-        </article>
+        </article> */}
       </div>
     </section>
   );
