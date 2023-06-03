fde38905ba5e989faacd9e76ef6a032468f38421
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-06-03 09:38:45 +0100

    updates on services component

--- new-portfolio/src/components/services/Services.jsx
+++ new-portfolio/src/components/services/Services.jsx
@@ -10,7 +10,7 @@ export const Services = () => {
       <h2>Services</h2>
       <div className="container services__container">
         <ListServices />
-        {/* <article className="service">
+        <article className="service">
           <div className="service__head">
             <h3>UI/UX Design</h3>
           </div>
@@ -66,7 +66,7 @@ export const Services = () => {
               </p>
             </li>
           </ul>
-        </article> */}
+        </article>
       </div>
     </section>
   );
