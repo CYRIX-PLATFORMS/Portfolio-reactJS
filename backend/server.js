d9b0f1313688fee066ff1e534445379c04b824ff
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-06-06 03:20:54 +0100

    Continuation of portfolio backend development

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,6 @@ const cors = require("cors");
 
 const app = express();
 
-//Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// routes
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
-
 const PORT = process.env.PORT || 5000;
 
 //connection to mongoDB and start server
